626e60e41cace1edf581b0245f365b176b05b7cd
ilCame <ale.camer.pv@gmail.com>
2022-01-13 12:10:45 +0100

    setup done

---
+++ afg-pet-clinic/src/test/java/guru/springframework/afgpetclinic/AfgPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.afgpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AfgPetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
