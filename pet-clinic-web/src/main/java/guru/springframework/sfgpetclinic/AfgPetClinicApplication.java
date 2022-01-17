ce82e40c60735aba007961297bdd72de9765fa72
ilCame <ale.camer'pv@gmail.com>
2022-01-17 12:13:46 +0100

    done some changes on the main

---
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/AfgPetClinicApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.sfgpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+public class AfgPetClinicApplication {
+
+	public static void main(String[] args) {SpringApplication.run(AfgPetClinicApplication.class, args);
+	}
+
+}
