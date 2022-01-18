0f85d1dd3661bb10f5317156ab35f71f9f2b595c
ilCame <ale.camer'pv@gmail.com>
2022-01-18 10:38:56 +0100

    lesson 61 done

--- pet-clinic-data/src/main/java/guru/springframework/mypetclinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/guru/springframework/mypetclinic/services/OwnerService.java
@@ -7,12 +7,4 @@ import java.util.Set;
 public interface OwnerService extends CrudeService<Owner, Long>{
 
     Owner findByLastName(String lastName);
-
-    /* possiamo direttamente cancellare questi metodi perche` vengono dierttamente implementati dall'interfaccia principale
-    Owner findById(Long id);
-
-    Owner save(Owner owner);
-
-    Set<Owner> findAll();
-     */
 }
