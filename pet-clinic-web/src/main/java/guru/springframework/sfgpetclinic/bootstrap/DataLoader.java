2479050bd56b679f4c4db4b615278de6593af392
ilCame <ale.camer'pv@gmail.com>
2022-01-19 12:56:18 +0100

    lesson 80, load data on startup, don't know what have i done but is okish

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/bootstrap/DataLoader.java
@@ -1,5 +1,7 @@
 package guru.springframework.sfgpetclinic.bootstrap;
 
+import guru.springframework.mypetclinic.model.Owner;
+import guru.springframework.mypetclinic.model.Vet;
 import guru.springframework.mypetclinic.services.OwnerService;
 import guru.springframework.mypetclinic.services.VetService;
 import guru.springframework.mypetclinic.services.map.OwnerMapService;
@@ -19,6 +21,36 @@ public class DataLoader implements CommandLineRunner {
     }
     @Override
     public void run(String... args) throws Exception {
+        Owner owner1 = new Owner();
+        owner1.setId(1L);
+        owner1.setFirstName("Micheal");
+        owner1.setLastName("Weston");
 
+        ownerService.save(owner1);
+
+        Owner owner2 = new Owner();
+        owner2.setId(2L);
+        owner2.setFirstName("Fiona");
+        owner2.setLastName("Glenanne");
+
+        ownerService.save(owner2);
+
+        System.out.println("Loaded Owners....");
+
+        Vet vet1 = new Vet();
+        vet1.setId(1L);
+        vet1.setFirstName("Sam");
+        vet1.setLastName("Axe");
+
+        vetService.save(vet1);
+
+        Vet vet2 = new Vet();
+        vet2.setId(2L);
+        vet2.setFirstName("Jessie");
+        vet2.setLastName("Porter");
+
+        vetService.save(vet2); //John actually put vet1 here but I am sure that was a mistake he didnt catch
+
+        System.out.println("Loaded Vets....");
     }
 }
