de985565151f62ba6e6d70bdf737c7edcdbb54ec
ilCame <ale.camer'pv@gmail.com>
2022-01-19 15:51:50 +0100

    lessons 82, 83, 84 done

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/OwnerController.java
@@ -1,13 +1,23 @@
 package guru.springframework.sfgpetclinic.controllers;
 
+import guru.springframework.sfgpetclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class OwnerController {
 
+    private final OwnerService ownerService;
+
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     @RequestMapping({"/owners", "/owners/index", "/owners/index.html"})
-    public String listOwner(){
+    public String listOwner(Model model){
+
+        model.addAttribute("owners", ownerService.findAll()); //il primo parametro e' il nome della proprieta' all'interno del modello, find all ci da un set capace di iterare
         return "owners/index";
     }
 }
