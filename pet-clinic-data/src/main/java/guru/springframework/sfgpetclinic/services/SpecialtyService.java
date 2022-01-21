bef144ca6fe71f6da9084c6b2486189eaf19cef6
ilCame <ale.camer'pv@gmail.com>
2022-01-21 12:56:31 +0100

    lesson 125 done, added the interface SpecialtuService and provided an implementation of it with the Speciality Map Service

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/SpecialtyService.java
@@ -0,0 +1,6 @@
+package guru.springframework.sfgpetclinic.services;
+
+import guru.springframework.sfgpetclinic.model.Specialty;
+
+public interface SpecialtyService extends CrudeService<Specialty, Long> {
+}
