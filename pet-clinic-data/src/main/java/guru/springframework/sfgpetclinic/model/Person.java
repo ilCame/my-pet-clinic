e0978a53e0499b78430ddd260e0f71aebec5186b
ilCame <ale.camer'pv@gmail.com>
2022-01-24 17:41:44 +0100

    lesson 147 done, Converted owners to a JPA entity

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Person.java
@@ -6,7 +6,7 @@ import javax.persistence.MappedSuperclass;
 @MappedSuperclass
 public class Person extends BaseEntity {
 
-    @Column(name = "first_name") // questo e` quello che ci aspettiamo dalla colonna del database
+    @Column(name = "first_name")
     private String firstName;
 
     @Column(name = "last_name")
