42dbd72d750a75f94e47e42f5308c3983900a0ff
ilCame <ale.camer'pv@gmail.com>
2022-01-24 17:25:22 +0100

    lesson 146 done, mapped BaseEntity as a MappedSuperClass

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/BaseEntity.java
@@ -1,8 +1,16 @@
 package guru.springframework.sfgpetclinic.model;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
+@MappedSuperclass //e` una annotazione speciale che permette di dire che e` una classe base per oggetti che derivano da essa
 public class BaseEntity implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     public Long getId() {
