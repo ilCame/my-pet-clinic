37064f070b6cfcb3005bce0548aad554e9656c9d
ilCame <ale.camer'pv@gmail.com>
2022-01-25 16:26:17 +0100

    lesson 183 done

--- pet-clinic-web/src/test/java/guru/springframework/afgpetclinic/AfgPetClinicApplicationTests.java
+++ pet-clinic-web/src/test/java/guru/springframework/afgpetclinic/AfgPetClinicApplicationTests.java
@@ -1,9 +1,15 @@
 package guru.springframework.afgpetclinic;
 
+import guru.springframework.sfgpetclinic.AfgPetClinicApplication;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@SpringBootTest
+@SpringBootTest(classes = AfgPetClinicApplication.class)
 public class AfgPetClinicApplicationTests {
+
+	@Test
 	public void contextLoads() {
 	}
 
